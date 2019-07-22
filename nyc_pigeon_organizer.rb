fdc05112da27db7a87c967a84d57cd69fecf4c8e
Daniel Ahn <93danielahn@gmail.com>
2019-07-22 19:29:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+        
+        if !pigeons[name]
+          pigeons[name] = {}
+        end
+
+        if !pigeons[name][key]
+          pigeons[name][key] = []
+        end
+
+        pigeons[name][key] << new_value.to_s
+
+      end
+    end
+  end
+  pigeons
 end
