ef312ae6225f5fe407ba50fa7cf9fe215aa5d676
Amy Resnik <aresnik11@gmail.com>
2019-07-26 01:19:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons = {}
+  data.keys.each do |item|
+    puts item
+  end
+  pigeons
 end
+
+
+#  data.each do |name, info|
+#    info.each do |innerName, innerInfo|
+#      innerInfo.each do |bird|
+#        if !pigeons[bird]
+#          pigeons[bird] = {:color => [], :gender => [], :lives => []}
+#        end
+#      end
+#    end
+#  end
+#  pigeons
+#end
