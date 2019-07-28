545ab4df3320896eca55be3ef387b782294e58d6
Eugenia Kim <elk303@nyu.edu>
2019-07-28 18:49:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |keys, value|
+    value.each do |trait, name|
+      name.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][keys] ||= []
+        pigeon_list[name][keys] << trait.to_s 
+    end
+  end
+end
+pigeon_list
+  
 end
