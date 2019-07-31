c902bf7e52e7aaa16bbf17035fc2b638416ae68f
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-31 00:59:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_org = {}
+  
+  data.each do |key, value|
+    value.each do |color, name|
+      name.each do |pig_name|
+        
+        if !new_pigeon_org[pig_name]
+          new_pigeon_org[pig_name] = {}
+        end
+        
+        if !new_pigeon_org[pig_name][key]
+          new_pigeon_org[pig_name][key] = []
+        end
+          new_pigeon_org[pig_name][key] << color.to_s
+      end
+    end
+  end
+  new_pigeon_org
 end
