d76c13740e87c7a6acb3b6ba1b7a90baad851ab8
Belinda Lei <belinda.lei95@gmail.com>
2019-08-04 06:55:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |category, attr|
+    attr.each do |desc, names|
+      names.each do |name|
+        hash[name] = {}
+      end
+    end
+  end
+  data.each do |category, attr|
+    attr.each do |desc, names|
+      names.each do |name|
+        if hash[name][category] #category found add desc
+          hash[name][category] << desc.to_s
+        else #add {category => [desc]}
+          hash[name][category] = [desc.to_s]
+        end
+      end
+    end
+  end
+  hash
 end
